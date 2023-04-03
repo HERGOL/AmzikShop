4cc41e3dc62c318685d2f339e8224b70cdafa129
Rafik Lamari <lamarirafik17@gmail.com>
2023-04-03 01:47:09 +0100

    rose ni

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -14,7 +14,6 @@ const BrickLayout = () => {
         
       </div>
       <div className={styles["brick-column"]}>
-        <img src="https://res.cloudinary.com/dmq21zp6g/image/upload/c_scale,h_549,q_80/9_e9tf4g.jpg" alt="" />
         <img src="https://res.cloudinary.com/dmq21zp6g/image/upload/c_scale,h_549,q_80/13_cjmod7.jpg" alt="" />
         <img src="https://res.cloudinary.com/dmq21zp6g/image/upload/c_scale,h_549,q_80/14_zz8usc.jpg" alt="" />
       </div>
