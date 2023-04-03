d191f183b3035156360a60ac0acc35c93a522ff0
Rafik Lamari <lamarirafik17@gmail.com>
2023-04-03 01:35:48 +0100

    img

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ import "./logo.png"
 const Nav = () => {
   return (
     <nav className={`${styles.nav} flex align-items-center`}>
-      <img src="https://res.cloudinary.com/dospfkcxd/image/upload/v1680481499/LOGO_fqencf.svg"/> 
+      <img className={`${styles.img} `} src="https://res.cloudinary.com/dospfkcxd/image/upload/v1680481499/LOGO_fqencf.svg"/> 
     </nav>
   )
 }
