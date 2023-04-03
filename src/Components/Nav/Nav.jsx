35c823ef8ec536f9b070ae3fb3272e70990961fb
Rafik Lamari <lamarirafik17@gmail.com>
2023-04-03 01:28:12 +0100

    svg added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ import "./logo.png"
 const Nav = () => {
   return (
     <nav className={`${styles.nav} flex align-items-center`}>
-      <img src="https://res.cloudinary.com/dmq21zp6g/image/upload/v1680479902/LOGO_dbbxma.svg/logo.png"/> 
+      <img src="https://res.cloudinary.com/dospfkcxd/image/upload/v1680481499/LOGO_fqencf.svg"/> 
     </nav>
   )
 }
