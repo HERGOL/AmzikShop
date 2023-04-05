5c29baa0d1484e7a6c6a2fd0778c087b36d72266
yidir <Yidirk@proton.me>
2023-04-05 14:25:29 +0200

    edit not found

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route path='/' component={Home} exact />
-        <Route component={NotFound} />
+        <Route path="*" component={NotFound} />
         <Route path='/article1' component={A1} exact />
         <Route path='/article2' component={A1} exact />
         <Route path='/article3' component={A1} exact />
